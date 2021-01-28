5eb5726f1f5aada960c9c7baf8a8a0bcb7cb0d1b
Jakob Löw <jakob@m4gnus.de>
2021-01-28 15:46:22 +0100

    initial commit (everything works, project can become abadonware now)

---
+++ change-notifier.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import json
+import base64
+import hashlib
+import requests
+import telegram
+
+with open("config.json", "r") as fd:
+	config = json.load(fd)
+with open("websites.json", "r") as fd:
+	websites = json.load(fd)
+
+bot = telegram.Bot(config["telegram-token"])
+changed = False
+
+for site in websites:
+	response = requests.get(site["url"])
+	content = response.text.encode("utf-8")
+
+	digest = hashlib.sha512(content).digest()
+	digest = base64.b64encode(digest).decode("ascii")
+
+	if site["hash"] != digest:
+		site["hash"] = digest
+		changed = True
+
+		msg = "{} changed!\n{}".format(site["name"], site["url"])
+		bot.send_message(config["telegram-chat"], msg)
+
+if changed:
+	with open("websites.json", "w") as fd:
+		json.dump(websites, fd, indent=4)
